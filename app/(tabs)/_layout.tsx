f2d3530eb8a9b93e31ff421e03220338e6c878c5
Nguyen Loc <locdedoda@gmail.com>
2026-08-28 22:41:56 +0700

    feat: add tabs layout and screens for settings and subscription details
    - Created a new TabsLayout component to manage tab navigation.
    - Added a Settings screen with a simple text display.
    - Implemented a StartPage that redirects to the tabs layout.
    - Added SubscriptionDetails screen to display subscription information based on the ID.

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,35 @@
+import { tabs } from "@/constants/data";
+import { clsx } from "clsx";
+import { Image } from "expo-image";
+import { Tabs } from "expo-router";
+import { View } from "react-native";
+
+const TabsLayout = () => {
+  const TabIcon = ({ focused, icon }: TabIconProps) => {
+    return (
+      <View className="tabs-icon">
+        <View className={clsx("tabs-pill", focused && "tabs-active")}>
+          <Image source={icon} className="tabs-glyph" />
+        </View>
+      </View>
+    );
+  };
+  return (
+    <Tabs screenOptions={{ headerShown: false }}>
+      {tabs.map((tab) => (
+        <Tabs.Screen
+          key={tab.name}
+          name={tab.name}
+          options={{
+            title: tab.title,
+            tabBarIcon: ({ focused }) => (
+              <TabIcon focused={focused} icon={tab.icon} />
+            ),
+          }}
+        />
+      ))}
+    </Tabs>
+  );
+};
+
+export default TabsLayout;
