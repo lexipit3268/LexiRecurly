5d8de15feaf1e80e559d39874ac3cb8b95962d8c
Nguyen Loc <locdedoda@gmail.com>
2026-09-02 22:42:08 +0700

    feat: enhance home screen layout with user info and balance display; add ListHeading component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,32 +1,43 @@
-import { Href, Link } from "expo-router";
+import ListHeading from "@/components/ListHeading";
+import { HOME_BALANCE, HOME_USER } from "@/constants/data";
+import { icons } from "@/constants/icons";
+import { formatCurrency } from "@/lib/utils";
+import dayjs from "dayjs";
 import { styled } from "nativewind";
-import { Text } from "react-native";
+import { Image, Text, View } from "react-native";
 import { SafeAreaView as RNSafreAreaView } from "react-native-safe-area-context";
-
 const SafeAreaView = styled(RNSafreAreaView);
 
 export default function App() {
   return (
     <SafeAreaView className="flex-1 bg-background p-5">
-      <Text className="text-7xl font-sans-extrabold">Hello</Text>
-      <Link
-        href={"/onboarding"}
-        className="px-6 py-4 rounded-md bg-primary font-sans-bold text-white"
-      >
-        Go to OnBoarding
-      </Link>
-      <Link
-        href={"/sign-in"}
-        className="px-6 py-4 rounded-md bg-primary font-sans-bold text-white"
-      >
-        Go to SignIn
-      </Link>
-      <Link
-        href={"/subscriptions/spotify" as Href}
-        className="px-6 py-4 rounded-md bg-primary font-sans-bold text-white"
-      >
-        Go to Spotify
-      </Link>
+      <View className="home-header">
+        <View className="home-user">
+          <Image
+            source={{
+              uri: "https://avatars.githubusercontent.com/u/62460969?s=512&v=4",
+            }}
+            className="home-avatar"
+          />
+          <Text className="home-user-name">{HOME_USER.name}</Text>
+        </View>
+        <Image source={icons.add} className="home-add-icon" />
+      </View>
+      <View className="home-balance-card">
+        <Text className="home-balance-label">Balance</Text>
+        <View className="home-balance-row">
+          <Text className="home-balance-amount">
+            {formatCurrency(HOME_BALANCE.amount)}
+          </Text>
+          <Text className="home-balance-date">
+            {dayjs(HOME_BALANCE.nextRenewalDate).format("MM/DD")}
+          </Text>
+        </View>
+      </View>
+      <View>
+        <ListHeading title="Lmao" />
+        <ListHeading title="Lmao" />
+      </View>
     </SafeAreaView>
   );
 }
